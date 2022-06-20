81ef4479f10193a2f762a6a5b27a4152726df0d6
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-06-20 12:57:39 -0500

    setting up BST class and magic methods

--- BST.py
+++ BST.py
@@ -1,4 +1,28 @@
 #Part 1: Create a BSTNode class
+class BSTNode:
+  def __init__(self, data = None, left = None, right = None):
+    self.data = data
+    self.left = left
+    self.right = right
+    
+  def __str__(self):
+    return str(self.data)
+
+  def __repr__(self):
+    return str(self.data)
+      
 
 #Part 2: Create a BST class
   #Part 3: Add functionality to your BST class
+
+
+node1 = BSTNode(3)
+print(node1) #3
+
+node2 = BSTNode(4, left=node1)
+print(node2) #4
+
+node3 = BSTNode()
+print(node3) #None
+node3.data = 5
+print(node3) #5
